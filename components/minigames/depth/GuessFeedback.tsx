84897735834e8e8233ac3387f03cce7e04cac779
Sean White <smw0616@gmail.com>
2025-07-16 21:36:51 -0700

    Animate depth game feedback

--- components/minigames/depth/GuessFeedback.tsx
+++ components/minigames/depth/GuessFeedback.tsx
@@ -8,7 +8,7 @@ function GuessFeedback({
   if (!result) return;
 
   return (
-    <div className="absolute left-1/2 top-[calc(50%+28px)] -translate-x-1/2 -translate-y-1/2 z-999">
+    <div className="absolute left-1/2 top-[calc(50%+28px)] -translate-x-1/2 -translate-y-1/2 z-999 animate-[ping_2s]">
       <div
         className={` rounded-full border-5 border-black ${
           result == "correct" ? "bg-green-500" : "bg-red-500"
